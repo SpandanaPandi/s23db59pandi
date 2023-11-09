068c9436d93cc67ea932553b65ffb298cb4443c8
SpandanaPandi <143204692+SpandanaPandi@users.noreply.github.com>
2023-11-08 23:17:46 -0600

    Added costumes

--- app.js
+++ app.js
@@ -4,11 +4,96 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mongoose = require("mongoose");
+var resourceRouter = require('./routes/resource');
+var costumeRouter = require('./routes/costumes');
+require('dotenv').config();
+const connectionString = process.env.MONGO_CON
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+ })
+ .then(() => console.log("Connected to MongoDB"))
+ .catch((err) => console.log("Error Connecting to MongoDB: ", err));
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function(){console.log("Connection to DB succeeded")})
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var transportationRouter = require('./routes/transportation');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
+var Costume = require('./models/costume');
+
+
+async function recreateDB(){
+
+  // Delete everything
+ 
+  await Costume.deleteMany();
+ 
+  let instance1 = new Costume(
+ 
+   {
+ 
+    costume_type: "Casual wear",
+ 
+    size:'Medium',
+ 
+    cost:20
+ 
+   });
+ 
+   let instance2 = new Costume(
+ 
+    {
+ 
+     costume_type: "party wear",
+ 
+     size: 'Medium',
+ 
+     cost: 89.4
+ 
+    });
+ 
+    let instance3 = new Costume(
+ 
+     {
+ 
+      costume_type: "Traditional wear",
+ 
+      size: 'Extra Small',
+ 
+      cost: 20.5
+ 
+     });
+ 
+  instance1.save()
+ 
+  .then(doc => {console.log("First object saved")})
+ 
+   .catch(err=>{console.error(err)})
+ 
+  instance2.save()
+ 
+  .then(doc => {console.log("Second object saved")})
+ 
+   .catch(err=>{console.error(err)})
+ 
+  instance3.save()
+ 
+  .then(doc => {console.log("Third object saved")})
+ 
+   .catch(err=>{console.error(err)})
+ 
+  }
+ 
+ let reseed = true;
+ if (reseed) {recreateDB();}
+
 var app = express();
 
 // view engine setup
@@ -26,6 +111,9 @@ app.use('/users', usersRouter);
 app.use('/transportation', transportationRouter);
 app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
+app.use("/resource", resourceRouter);
+app.use('/costumes', costumeRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
