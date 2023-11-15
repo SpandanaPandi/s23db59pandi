f36c5da7ba3945c386c9893296269743c45b2b83
SpandanaPandi <143204692+SpandanaPandi@users.noreply.github.com>
2023-11-15 17:28:46 -0600

    Added pug of upate,delete and create

--- controllers/transportation.js
+++ controllers/transportation.js
@@ -108,3 +108,47 @@ exports.transportation_view_one_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
     };
+
+    // Handle building the view for creating a costume.
+    // No body, no in path parameter, no query.
+    // Does not need to be async
+    exports.transportation_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('transportationcreate', { title: 'Transportation Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle building the view for updating a costume.
+// query provides the id
+exports.transportation_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Transportation.findById(req.query.id)
+    res.render('transportationupdate', { title: 'Transportation Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    // Handle a delete one view with id from query
+exports.transportation_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Transportation.findById(req.query.id)
+    res.render('transportationdelete', { title: 'Transportation Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
+    
+   
+    
--- routes/transportation.js
+++ routes/transportation.js
@@ -7,5 +7,11 @@ router.get('/', function(req, res, next) {
   res.render('transportation', { title: 'Search results of transportation' });
 });
 router.get('/detail', transportation_controller.transportation_view_one_Page);
- 
+/* GET create costume page */
+router.get('/create', transportation_controller.transportation_create_Page);
+/* GET create update page */
+router.get('/update', transportation_controller.transportation_update_Page);
+/* GET delete costume page */
+router.get('/delete', transportation_controller.transportation_delete_Page);
+
 module.exports = router;
