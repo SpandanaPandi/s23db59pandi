7e4691ee4fdc999ebac8ed0eef89bfc877b0dc02
SpandanaPandi <143204692+SpandanaPandi@users.noreply.github.com>
2023-11-17 11:48:29 -0600

    Added

--- routes/transportation.js
+++ routes/transportation.js
@@ -3,9 +3,7 @@ const transportation_controller= require('../controllers/transportation');
 var router = express.Router();
  
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('transportation', { title: 'Search results of transportation' });
-});
+router.get('/',transportation_controller.transportation_view_all_Page);
 router.get('/detail', transportation_controller.transportation_view_one_Page);
 /* GET create costume page */
 router.get('/create', transportation_controller.transportation_create_Page);
