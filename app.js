1e00ccd17a4a53d7c925c449bf9ff7e68f1c9923
SpandanaPandi <143204692+SpandanaPandi@users.noreply.github.com>
2023-11-29 00:49:19 -0600

    Added secured

--- app.js
+++ app.js
@@ -3,72 +3,81 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-const mongoose = require("mongoose");
-var resourceRouter = require('./routes/resource');
-//var costumeRouter = require('./routes/costumes');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var Account = require('./models/account');
+var transportation=require('./models/transportation');
+var mongoose = require('mongoose');
 require('dotenv').config();
-const connectionString = process.env.MONGO_CON
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
- })
- .then(() => console.log("Connected to MongoDB"))
- .catch((err) => console.log("Error Connecting to MongoDB: ", err));
-//Get the default connection
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({ username: username })
+      .then(function(user) {
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function(err) {
+        return done(err);
+      });
+  }
+));
+
+const connectionString = process.env.MONGO_CON;
+mongoose.connect(connectionString);
 var db = mongoose.connection;
-//Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){console.log("Connection to DB succeeded")})
+db.once("open", function() {
+  console.log("Connection to DB succeeded");
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var transportationRouter = require('./routes/transportation');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
-var transportation = require('./models/transportation');
+var resourceRouter = require('./routes/resource');
 
-//lab-13
+var app = express();
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-  Account.findOne({ username: username })
-  .then(function (user){
-  if (err) { return done(err); }
-  if (!user) {
-  return done(null, false, { message: 'Incorrect username.' });
-  }
-  if (!user.validPassword(password)) {
-  return done(null, false, { message: 'Incorrect password.' });
-  }
-  return done(null, user);
-  })
-  .catch(function(err){
-  return done(err)
-  })
-  })
-  )
-  app.use(require('express-session')({
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  
-  // passport config
-// Use the existing connection
-// The Account model
-var Account =require('./models/account');
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/transportation', transportationRouter);
+app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
+app.use('/resource', resourceRouter);
+
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
 
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-async function recreateDB(){
-
-  // Delete everything
- 
+async function recreateDB() {
   await transportation.deleteMany();
  
   let instance1 = new transportation(
@@ -114,42 +123,15 @@ async function recreateDB(){
    .catch(err=>{console.error(err)})
  
   }
- 
- let reseed = true;
- if (reseed) {recreateDB();}
 
-var app = express();
+let reseed = true;
+if (reseed) {
+  recreateDB();
+}
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/transportation', transportationRouter);
-app.use('/board', boardRouter);
-app.use('/choose', chooseRouter);
-app.use("/resource", resourceRouter);
-//app.use('/costumes', costumeRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/transportation.js
+++ routes/transportation.js
@@ -1,15 +1,23 @@
 var express = require('express');
+var passport = require('passport');
 const transportation_controller= require('../controllers/transportation');
 var router = express.Router();
- 
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 /* GET home page. */
 router.get('/',transportation_controller.transportation_view_all_Page);
 router.get('/detail', transportation_controller.transportation_view_one_Page);
 /* GET create costume page */
 router.get('/create', transportation_controller.transportation_create_Page);
 /* GET create update page */
-router.get('/update', transportation_controller.transportation_update_Page);
+router.get('/update', secured, transportation_controller.transportation_update_Page);
 /* GET delete costume page */
 router.get('/delete', transportation_controller.transportation_delete_Page);
-
-module.exports = router;
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });
+module.exports = router; 
