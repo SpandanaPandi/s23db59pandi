9744ca9310b846ad73836d4812fde9ec63455a9a
SpandanaPandi <143204692+SpandanaPandi@users.noreply.github.com>
2023-11-29 18:00:30 -0600

    Added buttons

--- models/transportation.js
+++ models/transportation.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose")
 const transportationSchema = mongoose.Schema({
     TransportationType: String,
     Destination: String,
-    Price: Number
-})
+    Price: {
+        type: Number,
+    min: [0, 'Price must be greater than or equal to 0'], // Minimum price allowed
+    max: [1000, 'Price cannot exceed 1000'] // Maximum price allowed
+}}
+)
 module.exports = mongoose.model("Transportation",
     transportationSchema)
